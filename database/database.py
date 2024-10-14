dd9c16eab8676198d1963e50956631355779f296
rohit870891 <koyeb77user@gmail.com>
2024-10-14 22:46:07 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -105,3 +105,43 @@ async def full_adminbase():
     user_docs = admin_data.find()
     user_ids = [int(doc['_id']) async for doc in user_docs]
     return user_ids
+
+async def add_premium(user_id, time_limit_months):
+    expiration_timestamp = int(time.time()) + time_limit_months * 30 * 24 * 60 * 60
+    premium_data = {
+        "user_id": user_id,
+        "expiration_timestamp": expiration_timestamp,
+    }
+    collection.insert_one(premium_data)
+    dbclient.close()
+
+async def remove_premium(user_id):
+    result = collection.delete_one({"user_id": user_id})
+    dbclient.close()
+
+async def remove_expired_users():
+    current_timestamp = int(time.time())
+
+    # Find and delete expired users
+    expired_users = collection.find({"expiration_timestamp": {"$lte": current_timestamp}})
+
+    for expired_user in expired_users:
+        user_id = expired_user["user_id"]
+        collection.delete_one({"user_id": user_id})
+
+    dbclient.close()
+
+async def list_premium_users():
+
+    premium_users = collection.find({})
+
+    premium_user_list = []
+
+    for user in premium_users:
+        user_id = user["user_id"]
+        user_info = Bot.get_users(user_id)
+        username = user_info.username if user_info.username else user_info.first_name
+        expiration_timestamp = user["expiration_timestamp"]
+        premium_user_list.append(f"{user_id} - {username} - Expiration Timestamp: {expiration_timestamp}")
+
+    return premium_user_list
